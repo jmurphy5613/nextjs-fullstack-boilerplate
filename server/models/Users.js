2fbc3c09675d0669830020b28a37d67cbb23fa60
jmurphy5613 <jmurphy5613@gmail.com>
2022-02-01 09:12:19 -0500

    added user model

---
+++ server/models/Users.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+    return User;
+}
